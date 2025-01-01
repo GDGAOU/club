57778345efd97acafe89113036fd22a0f8a97228
GDGAOU <gdgaou1@gmail.com>
2025-01-01 18:16:05 +0300

    fix: update prisma exports to use named exports

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,12 +6,10 @@ declare global {
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-const prisma =
+export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ['query'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
